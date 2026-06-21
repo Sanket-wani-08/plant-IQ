debb8cfef31de348625ff467f44279315b2111fe
Sanket-wani-08 <sanketwani810@gmail.com>
2026-06-21 16:23:41 +0530

    Increase JSON body limit to 50mb

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(
   })
 );
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 // API Routes
 app.use("/api", plantRoutes);
